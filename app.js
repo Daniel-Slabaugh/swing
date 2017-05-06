1a0516222cdd3ef87750fa6e0281ae6e2b47f066
Patrick Ford <patrickford@msn.com>
2017-05-06 09:38:45 -0700

    Added submit handler for login

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
-
+  $("#sign-in").submit(function(e) {
+    e.preventDefault();
+    var email = $("#email").val();
+    var password = $("#password").val();
+    console.log(email, password);
+  });
 
 });
