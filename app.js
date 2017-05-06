c5f2efcbab1ee5a4764b72a358116e4f123dc0b4
Patrick Ford <patrickford@msn.com>
2017-05-06 09:46:14 -0700

    Added search page

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
 
+  $("#search-page").hide();
+
   $("#sign-in").submit(function(e) {
     e.preventDefault();
     var email = $("#email").val();
     var password = $("#password").val();
     console.log(email, password);
+    $("#search-page").show();
+    $("#sign-in-page").hide();    
   });
 
+  $("#log-out").click(function() {
+    $("#search-page").hide();
+    $("#sign-in-page").show();
+  })
+  
 });
