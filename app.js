1623a21ab4ae9b3216cffbc34f263ab7b6141423
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-05-30 10:39:10 -0500

    mentor session changes

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ $(document).ready(function() {
 
   $("#search-page").hide();
 
-  $("#sign-in").submit(function(e) {
+  $("#start").submit(function(e) {
     e.preventDefault();
     var email = $("#email").val();
     var password = $("#password").val();
     console.log(email, password);
     $("#search-page").show();
-    $("#sign-in-page").hide();    
+    $("#welcome-page").hide();    
   });
 
   $("#log-out").click(function() {
     $("#search-page").hide();
-    $("#sign-in-page").show();
+    $("#welcome-page").show();
   })
   
 });
